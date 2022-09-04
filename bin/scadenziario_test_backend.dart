9f8574e2f7748a20c6623af88b1e427f535e08a2
Antonio Fasolato <antonio.fasolato@gmail.com>
2022-09-04 14:48:49 +0200

    Created empty dart project

---
+++ bin/scadenziario_test_backend.dart
@@ -0,0 +1,5 @@
+import 'package:scadenziario_test_backend/scadenziario_test_backend.dart' as scadenziario_test_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${scadenziario_test_backend.calculate()}!');
+}
